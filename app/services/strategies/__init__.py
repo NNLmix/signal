ef743436a7384121ad3b59e5dc48266fe167416b
MarkL1N <83065322+NNLmix@users.noreply.github.com>
2025-09-24 12:51:19 +0300

    -

--- app/services/strategies/__init__.py
+++ app/services/strategies/__init__.py
@@ -1,5 +1,4 @@
 from .btc_price_threshold import Strategy as BtcPriceThreshold
-from .scalp_ema import Strategy as ScalpEMA
 from .trend_pullback import Strategy as TrendPullback
 
-STRATEGIES = [BtcPriceThreshold(), ScalpEMA(), TrendPullback()]
+STRATEGIES = [BtcPriceThreshold(), TrendPullback()]
